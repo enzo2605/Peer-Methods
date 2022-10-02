ad72e5d2c109d814a4155194a1dbcb38df54691d
Enzo2605 <enzoiannucci2@gmail.com>
2022-10-02 10:09:50 +0200

    packThreeVector OK

--- peer_methods_C/main.c
+++ peer_methods_C/main.c
@@ -17,10 +17,10 @@
 #define D  0.802
 
 int main(int argc, char *argv[]) {
-    // Randomly initialization for the seed
+    // Random initialization for the seed
     srand((unsigned int)time(NULL));
-    
-    // intervals
+
+    // Intervals
     double t_start, t_end, x_start, x_end;
 
     // Checking arguments passed by the user
@@ -42,7 +42,9 @@ int main(int argc, char *argv[]) {
     // Printing the interval used
     printf("Interval used:\ntime span: [%f, %f]\nspace span: [%f, %f]\n", t_start, t_end, x_start, x_end);
 
-    // Time initialization
+    /*********************************************** 
+     *          Time initialization 
+     * *********************************************/
     double t_span[2] = { t_start, t_end };
     double Delta_t = 1.0f / pow(2.0f, 11.0f);
 
@@ -51,7 +53,9 @@ int main(int argc, char *argv[]) {
     t_int = setDVector(t_int, t_start, t_end, Delta_t, &n_points_t);
     int N = (t_span[1] - t_span[0]) / Delta_t;
 
-    // Space initialization
+    /*********************************************** 
+     *          Space initialization 
+     * *********************************************/
     int M = 64;
     double x_span[2] = { x_start, x_end };
     double Delta_x = (x_span[1] - x_span[0]) / M;
@@ -60,8 +64,10 @@ int main(int argc, char *argv[]) {
     int n_points_x;
     x_int = setDVector(x_int, x_start, x_end, Delta_x, &n_points_x);
 
-    /*** Define initial conditions ***/
-    // u10_time
+    /*********************************************** 
+     *          Define initial conditions 
+     * *********************************************/
+    /**** u10_time *****/
     // Allocation of the vector
     double *tempVecU10 = (double *)Calloc(M, sizeof(double));
     // Initialization with random values between 0 and 1
@@ -70,9 +76,8 @@ int main(int argc, char *argv[]) {
     cblas_dscal(M, 0.7, tempVecU10, 1);
     // Add the scalar alpha at every element of the array tempVecU10
     double *u10_time = sumScalarByVector(tempVecU10, M, 1.4f);
-    printDVector(u10_time, M);
 
-    // u20_time
+    /**** u20_time *****/
     // Allocation of the vector
     double *tempVecU20 = (double *)Calloc(M, sizeof(double));
     // Initialization with random values between 0 and 1
@@ -81,9 +86,8 @@ int main(int argc, char *argv[]) {
     cblas_dscal(M, 0.7, tempVecU20, 1);
     // Add the scalar alpha at every element of the array tempVecU20
     double *u20_time = sumScalarByVector(tempVecU20, M, 1.4f);
-    //printDVector(u20_time, M);
 
-    // w0_time
+    /**** w0_time *****/
     // Allocation of the vector
     double *tempVecW0 = (double *)Calloc(M, sizeof(double));
     // Initialization with random values between 0 and 1
@@ -92,18 +96,16 @@ int main(int argc, char *argv[]) {
     cblas_dscal(M, 0.07, tempVecW0, 1);
     // Add the scalar alpha at every element of the array tempVecW0
     double *w0_time = sumScalarByVector(tempVecW0, M, 0.14f);
-    //printDVector(w0_time, M);
 
-    // Create vector y0 = [U10;U20;W0] with initial conditions
-    /*
-        y0 = zeros(3*M,1);
-        y0(1:M,1) = u10_time(1:M,1);
-        y0(M+1:2*M,1) = u20_time(1:M,1);
-        y0(2*M+1:3*M,1) = w0_time(1:M,1);
-    */
+    /************************************************************** 
+     *  Create vector y0 = [U10;U20;W0] with initial conditions 
+     * ***********************************************************/
+    int y0Dimension;
+    double *y0 = packThreeVectors(M, u10_time, u20_time, w0_time, &y0Dimension);
 
-
-    // Finite differences of order two and periodic boundary conditions
+    /********************************************************************
+     * Finite differences of order two and periodic boundary conditions
+     * *****************************************************************/
     /*
         Ldiff = (1/Delta_x^2)*(-2*eye(M)+diag(ones(M-1,1),1)+diag(ones(M-1,1),-1));
         Ldiff(1,M) = 1/Delta_x^2; Ldiff(M,1) = 1/Delta_x^2;
