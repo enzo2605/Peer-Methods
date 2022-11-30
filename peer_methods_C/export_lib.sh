62a677a1a64c5f7143d4e7a69452121fc8cfc9cf
enzo2605 <enzoiannucci2@gmail.com>
2022-11-30 08:36:55 +0100

    Updated configuration file

--- peer_methods_C/export_lib.sh
+++ peer_methods_C/export_lib.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vincenzo/Universita/Peer-Methods/peer_methods_C/peerMethods/lib
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/vincenzo/Universita/Peer-Methods/peer_methods_C/peerMethods/lib' >> ~/.bashrc
